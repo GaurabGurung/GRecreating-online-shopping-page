a5509932d8b9b660b106f323877957c9272c3054
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-08-23 23:46:38 +1000

    creating shopping page and adding products

--- src/routes/Shop/shop.component.jsx
+++ src/routes/Shop/shop.component.jsx
@@ -1,10 +1,19 @@
 import './shop.styles.scss';
+import { useContext } from 'react';
+import { ProductsContext } from '../../contexts/products.context';
+import ProductCard from '../../components/Product-card/product-card.component';
 
 const Shop = () => {
+    const {products} = useContext(ProductsContext);
 
     return (
-        <div>
-            I am shopping page
+        <div className='products-container'>
+            {products.map((product)=>{
+
+                return (
+                    <ProductCard key={product.id} product={product}/>
+                )
+            })}
         </div>
     )
 }
