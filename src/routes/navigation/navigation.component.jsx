806446a270d02505f7fc24b5a948f7f693471469
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-08-29 21:25:10 +1000

    adding the cartdropDownMenu to our app

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,16 @@
 import { ReactComponent as CrwnLogo} from '../../assests/crown.svg'
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { Link, Outlet } from 'react-router-dom';
 import './navigation.styles.scss';
 import { Fragment, useContext, useEffect } from 'react';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
-
+import { CartContext } from '../../contexts/cart.context';
 const Navigation = () => {
 
     const {currentUser } = useContext (UserContext);
-
+    const { isCartOpen} =useContext(CartContext)
     return (
         <Fragment>
           <div className="navigation">
@@ -31,7 +32,7 @@ const Navigation = () => {
               )}
             <CartIcon />
             </div>
-
+            {isCartOpen && <CartDropdown />}
           </div>
           <Outlet/>
         </Fragment>
