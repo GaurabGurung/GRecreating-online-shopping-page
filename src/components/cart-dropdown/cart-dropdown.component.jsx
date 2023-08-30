5409b03e3cdeadabd8e0c8f1df06aab7eccb1bd9
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-08-30 23:54:56 +1000

    start to create a function to add the product items to the cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,14 @@
 import Button from '../button/button.component.jsx'
 import './cart-dropdown.styles.scss'
+import CartItem from '../cart-item/cart-item.component.jsx'
 
 const CartDropdown = () => {
 
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'/>
+            <div className='cart-items'>
+                {[].map ( item => <CartItem cartItem= {item}/>)}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
