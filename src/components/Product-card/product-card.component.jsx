058d85eeb9bc184e6977120f0c12904ada6a2786
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-09-03 17:46:34 +1000

    added add to cart feature in our app

--- src/components/Product-card/product-card.component.jsx
+++ src/components/Product-card/product-card.component.jsx
@@ -5,9 +5,11 @@ import { CartContext } from '../../contexts/cart.context';
 
 const ProductCard = ({product}) => {
     const {name, imageUrl,  price} = product;
-    const { addItemToCart } = useContext(CartContext);
-    const addProductToCart = ()=> addItemToCart(product);
+    const { addItemToCart, setCartCount, cartCount } = useContext(CartContext);
 
+    const addProductToCart = ()=> {
+        addItemToCart(product);
+    }
     return (
         <div className='product-card-container'>
             <img src={imageUrl} alt={`${name}`}/>
